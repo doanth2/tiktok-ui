80133f5a3aa5e14f07078e3918ceda8e63890967
doanth2 <doanthieuchinh@gmail.com>
2022-09-20 17:23:57 +0900

    doan

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <div>Nguyen van doan</div>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
